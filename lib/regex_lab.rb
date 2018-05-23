c6a0d39c31ba592127fa8db0c901ff932e3775d5
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-23 01:26:38 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,36 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  #binding.pry
+  if word.match(/\A[aeiouAEIOU]\w+/).nil?
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  #binding.pry
+  return text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  return text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(/\b[A-Z]\w*[.!?,]\W/).nil?
+    return false
+  else
+    return true
+  end
 
 end
 
 def valid_phone_number?(phone)
-
+  #binding.pry
+  if phone.match(/\b\d{3}-?\d{3}-?\d{4}\b/).nil?
+    return false
+  else
+    return true
+  end
 end
